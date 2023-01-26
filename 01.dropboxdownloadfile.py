aed64034c16f552d41af097d1c51faa6ed385972
vishnuiot <vishnuprasad.iit@gmail.com>
2023-01-26 01:33:50 +0100

    modification

--- 01.dropboxdownloadfile.py
+++ 01.dropboxdownloadfile.py
@@ -1,1 +1,6 @@
 import dropbox
+access_token = '<token value here>'
+dbx = dropbox.Dropbox(access_token)
+with open("C:\Test.txt", "w") as f:
+metadata, res = dbx.files_download(path="/Test.txt")
+f.write(res.content)
