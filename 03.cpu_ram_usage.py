822be5d038d7e2da629afd0fd98014c886864683
vishnuiot <vishnuprasad.iit@gmail.com>
2023-01-31 22:00:04 +0100

    modification

---
+++ 03.cpu_ram_usage.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import psutil
+# gives a single float value
+cpu_usage=psutil.cpu_percent()
+# gives an object with many fields
+psutil.virtual_memory()
+# you can convert that object to a dictionary 
+dict(psutil.virtual_memory()._asdict())
+# you can have the percentage of used RAM
+cpu_mem=psutil.virtual_memory().percent
+79.2
+# you can calculate percentage of available memory
+mem_avail=psutil.virtual_memory().available * 100 / psutil.virtual_memory().total
+20.8
+
+print("CPU_RAM usage  = ",cpu_mem)
+print("CPU_usage = ",cpu_usage)
+print("available memory =",mem_avail)
