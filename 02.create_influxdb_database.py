beb574f61975d8fb2af5171e61aede60b131e628
vishnuiot <vishnuprasad.iit@gmail.com>
2023-01-31 21:18:46 +0100

    modification

---
+++ 02.create_influxdb_database.py
@@ -0,0 +1,18 @@
+from influxdb import InfluxDBClient
+
+
+
+
+json_body = [
+    {
+        "measurement": "cpu_load_short",
+        "tags": {
+            "host": "server01",
+            "region": "us-west"
+        },
+        "time": "2009-11-10T23:00:00Z",
+        "fields": {
+            "value": 0.64
+        }
+    }
+]
