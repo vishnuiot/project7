1548bc4ab4b22c135af85b3dab0809a5d4cec2f2
vishnuiot <vishnuprasad.iit@gmail.com>
2023-08-27 20:48:17 +0200

    modification

---
+++ 01.gitupload.sh
@@ -0,0 +1,31 @@
+#step 1: synchronize remote branch with local folder 
+git remote add origin git@github.com:vishnuiot/project7
+git pull origin main
+
+# upload contents of local folder into remote repository
+git add .
+git commit -m "modification"
+git push origin main
+# git remote add origin git@github.com:vishnuiot/github_instructions
+#step 3 : git add and commit locally + remote
+# git add 
+# git commit -m "modification"
+
+
+# Step 1: check main branch locally
+# git checkout main
+# Step 2: list branches locally
+# git branch -a
+
+
+# # Step3 : create a  branch called myfeature
+# git checkout -b 01.August main
+
+# #step 6: Pull code into remoteserver-branch 
+# git checkout 01.August
+# git pull origin 01.August
+
+
+
+# # Step 4: Add files 
+
