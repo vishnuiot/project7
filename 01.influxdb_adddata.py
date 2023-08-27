c9684d6fa3e133ce339d6876e17c9c98b2c5144f
vishnuiot <vishnuprasad.iit@gmail.com>
2023-08-27 20:51:49 +0200

    modification

---
+++ 01.influxdb_adddata.py
@@ -0,0 +1,30 @@
+import influxdb_client, os, time
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+from influxdb_client import WriteApi, WriteOptions
+
+token = os.environ.get("INFLUXDB_TOKEN")
+org = "demo"
+url = "http://localhost:8086"
+bucket="demo_bucket"
+
+client = influxdb_client.InfluxDBClient(
+   url=url,
+   token=token,
+   org=org
+)
+write_api = client.write_api(write_options=SYNCHRONOUS)
+# p = influxdb_client.Point("my_measurement").tag("location", "Prague").field("temperature", 25.3)
+# write_api.write(bucket=bucket, org=org, record=p)
+
+for value in range(5):
+  point = (
+    Point("measurement1")
+    .tag("tagname1", "tagvalue1")
+    .field("field1", value)
+  )
+  write_api.write(bucket=bucket, org="demo", record=point)
+  time.sleep(1) # separate points by 1 second
+
+# p = influxdb_client.Point("my_measurement").tag("location", "Prague").field("temperature", 25.3)
+# write_api.write(bucket=bucket, org=org, record=p)
