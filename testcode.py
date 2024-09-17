38ce6e3b029caf0423794049794e9be68c1c4401
vishnuiot <vishnuprasad.iit@gmail.com>
2024-09-17 17:58:53 +0200

    test

--- testcode.py
+++ testcode.py
@@ -1,3 +1,8 @@
-fruits = ["apple", "banana", "cherry"]
-for x in fruits:
-  print(x)
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   print(x)
+  
+# import subprocess
+# subprocess.run(['./export_api.sh'])
+import os
+os.system('export_api.sh')
