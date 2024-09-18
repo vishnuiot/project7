78d18f39e7f26d39bef787994d9cabb0f9fc2aba
vishnuiot <vishnuprasad.iit@gmail.com>
2024-09-18 18:21:50 +0200

    test

--- 009.influxdb_temp_upload.py
+++ 009.influxdb_temp_upload.py
@@ -33,6 +33,8 @@ import influxdb_client, os, time
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
+
+
 token = os.environ.get("INFLUXDB_TOKEN")
 org    = "IBM"
 url    = "http://localhost:8086"
--- testcode.py
+++ testcode.py
@@ -2,7 +2,6 @@
 # for x in fruits:
 #   print(x)
   
-# import subprocess
-# subprocess.run(['./export_api.sh'])
-import os
-os.system('export_api.sh')
+
+import subprocess
+subprocess.call(['bash', './export_api.sh'])
