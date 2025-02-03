22300715447f3c086dc0abfd190aededbe21544c
vishnuiot <vishnuprasad.iit@gmail.com>
2025-02-03 22:39:32 +0530

    test

--- 009b.cpu_temp_usage_upload_influxdb.py
+++ 009b.cpu_temp_usage_upload_influxdb.py
@@ -16,6 +16,21 @@ cpu_cores_type=type(cpu_cores)
 print("The type of memory,cpu cores=",type(memory),type(cpu_cores))
 print("The type of memory={} ,cpu cores={}".format (memory_type,cpu_cores_type))
 
+t=psutil.sensors_temperatures()
+coretemp1=t["coretemp"][1][1]
+coretemp2=t["coretemp"][2][1]
+coretemp3=t["coretemp"][3][1]
+coretemp4=t["coretemp"][4][1]
+print("core 0 temperature =",t["coretemp"][1][1])
+print("core 1 temperature =",t["coretemp"][2][1])
+print("core 2 temperature =",t["coretemp"][3][1])
+print("core 3 temperature =",t["coretemp"][4][1])
+temperature_type=type(t["coretemp"][1][1])
+
+
+print("The type of memory,cpu cores=",type(memory),type(cpu_cores))
+print("The type of memory={} ,cpu cores={}".format (memory_type,cpu_cores_type))
+print("The type of memory={} ,cpu cores={} ,temperature={}".format (memory_type,cpu_cores_type,temperature_type))
 
 
 #Influxdb Section to upload CPU,Temp
@@ -38,10 +53,15 @@ bucket="cpu"
 client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
 write_api = client.write_api(write_options=SYNCHRONOUS)
 
-point = (Point("cpu_memory").tag("memorytag", "memorytagvalue").field("cpumemoryvalue",memory))
-point = (Point("cpu_memory").tag("corestag", "corestagvalue").field("corecountvalue",cpu_cores))
+point = (Point("cpu_parameters").tag("memorytag", "memorytagvalue").field("cpumemoryvalue",memory))
+point = (Point("cpu_parameters").tag("corestag", "corestagvalue").field("corecountvalue",cpu_cores))
+point = (Point("cpu_parameters").tag("coretemp1tag", "coretemp1tagvalue").field("coretemp_1_value",coretemp1))
+point = (Point("cpu_parameters").tag("coretemp2tag", "coretemp2tagvalue").field("coretemp_2_value",coretemp2))
+point = (Point("cpu_parameters").tag("coretemp3tag", "coretemp3tagvalue").field("coretemp_3_value",coretemp3))
+point = (Point("cpu_parameters").tag("coretemp4tag", "coretemp4tagvalue").field("coretemp_4_value",coretemp4))
+
 
 write_api = client.write_api(write_options=SYNCHRONOUS)
 write_api.write(bucket=bucket, org="zurich", record=point)
-time.sleep(1) # separate points by 1 second
+time.sleep(5) # separate points by 1 second
 
