7db5d2f26f8f2743cf3d2da1d910e91026a2302a
vishnuiot <vishnuprasad.iit@gmail.com>
2025-02-04 20:33:21 +0530

    test

--- 009c.cpu_parameters_dataframe.py
+++ 009c.cpu_parameters_dataframe.py
@@ -7,12 +7,7 @@ from datetime import datetime
 now = datetime.now()
 print(now)
 
-t=psutil.sensors_temperatures()
-# conversion of tuple to integer
-core_0=int(t["coretemp"][1][1])
-core_1=int(t["coretemp"][2][1])
-core_2=int(t["coretemp"][3][1])
-core_3=int(t["coretemp"][4][1])
+t=psutil.sensors_temperatures()  # Temp tuple to be converted to integer
 
 data = {
   "time":now ,
@@ -34,5 +29,6 @@ data = {
 
 #load data into a DataFrame object:
 df = pd.DataFrame(data)
-
 print(df) 
+# append data frame to CSV file
+df.to_csv('system_data.csv', mode='a', index=False, header=False)
